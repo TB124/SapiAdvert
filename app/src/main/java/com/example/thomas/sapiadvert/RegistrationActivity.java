2d5bea3bb470ff69ac0b36dd1a053364572d59e5
Akarmi1 <polymorphism96@gmail.com>
2017-12-23 22:51:46 +0200

    Profile pic upload,data upload to firebase database

--- app/src/main/java/com/example/thomas/sapiadvert/RegistrationActivity.java
+++ app/src/main/java/com/example/thomas/sapiadvert/RegistrationActivity.java
@@ -2,23 +2,37 @@ package com.example.thomas.sapiadvert;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int GALLERY_INTENT =3 ;
     private Button registerButton;
     private EditText emailInput;
     private EditText passwordInput;
@@ -28,6 +42,24 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
 
     private FirebaseAuth firebaseAuth;
 
+    ///
+    private ImageView profilePictureInput;
+    private EditText firstNameInput;
+    private EditText lastNameInput;
+    private EditText phoneNumberInput;
+    private EditText confirmPasswordInput;
+
+    private String emailAddress;
+    private String password;
+    private String passwordConfirm;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    ///
+    //Storage
+    private StorageReference firebaseStorage;
+    private Uri uri;
+    ///
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +81,25 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
 
         registerButton.setOnClickListener(this);
         signInTextView.setOnClickListener(this);
+
+        ///
+        profilePictureInput=(ImageView) findViewById(R.id.profilePictureInput);
+        firstNameInput=(EditText) findViewById(R.id.firstNameInput);
+        lastNameInput=(EditText) findViewById(R.id.lastNameInput);
+        phoneNumberInput=(EditText) findViewById(R.id.phoneNumberInput);
+        confirmPasswordInput=(EditText) findViewById(R.id.confirmPasswordInput);
+
+        profilePictureInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,GALLERY_INTENT);
+            }
+        });
+        ///Storage
+        firebaseStorage= FirebaseStorage.getInstance().getReference();
+
     }
 
     @Override
@@ -64,17 +115,41 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
     }
 
     private void registerUser() {
-        String emailAddress = emailInput.getText().toString().trim();
-        String password = passwordInput.getText().toString().trim();
+        emailAddress = emailInput.getText().toString().trim();
+        password = passwordInput.getText().toString().trim();
+        passwordConfirm=confirmPasswordInput.getText().toString().trim();
+        firstName=firstNameInput.getText().toString().trim();
+        lastName=lastNameInput.getText().toString().trim();
+        phoneNumber=phoneNumberInput.getText().toString().trim();
 
-        if (TextUtils.isEmpty(emailAddress)){
+        if(uri ==null){
+            Toast.makeText(this, "Please select a profile picture !",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(firstName)){
+            Toast.makeText(this, "Please enter your first name!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(lastName)){
+            Toast.makeText(this, "Please enter your last name!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(emailAddress)||(!android.util.Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches())){
             // email field is empty -- display message and return
-            Toast.makeText(this, "Please enter an email address!",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter a valid email address!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(phoneNumber)|| (!PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber))){
+            Toast.makeText(this, "Please enter a valid phone number!",Toast.LENGTH_SHORT).show();
             return;
         }
         if (TextUtils.isEmpty(password)){
             // password field is empty -- display message and return
             Toast.makeText(this, "Please enter a password!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(!password.equals(passwordConfirm)){
+            Toast.makeText(this, "Password confirmation failed !",Toast.LENGTH_SHORT).show();
             return;
         }
 
@@ -93,11 +168,35 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
                             Toast.makeText(RegistrationActivity.this, "You have successfully registered!",
                                     Toast.LENGTH_SHORT).show();
                             progressDialog.dismiss();
+
+                            //Database
+                            FirebaseUser currentUser=firebaseAuth.getCurrentUser();
+                            HashMap<String,String> datas=new HashMap<>();
+                            datas.put("FirstName",firstName);
+                            datas.put("LastName",lastName);
+                            datas.put("EmailAddress",emailAddress);
+                            datas.put("PhoneNumber",phoneNumber);
+                            DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().
+                                    child("Users")
+                                    .child(currentUser.getUid());
+                            databaseReference.setValue(datas);
+                            StorageReference filepath=firebaseStorage.child("ProfilePictures").child(currentUser.getUid());
+                            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    Toast.makeText(RegistrationActivity.this,"Profile pic upload succes !",Toast.LENGTH_LONG).show();
+                                }
+                            });
+
+
+
+                            ///
                             startMainActivity();
                         }
                         else{
                             // Registration was not successful
-                            Toast.makeText(RegistrationActivity.this, "Registration failed!",
+
+                            Toast.makeText(RegistrationActivity.this, "Registration failed! Error:"+ task.getException().getMessage(),
                                     Toast.LENGTH_SHORT).show();
                             progressDialog.dismiss();
                         }
@@ -108,5 +207,21 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
     private void startMainActivity(){
         finish();
         startActivity(new Intent(this, MainActivity.class));
+    }
+    //Image select
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == GALLERY_INTENT) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                uri=data.getData();
+               // StorageReference filepath=firebaseStorage.child("ProfilePictures").
+                //Glide.with(this).load(uri).into(profilePictureInput);
+                Glide.with(RegistrationActivity.this).load(uri).into(profilePictureInput);
+                //profilePictureInput.setImageURI(uri);
+
+            }
+        }
     }
 }
